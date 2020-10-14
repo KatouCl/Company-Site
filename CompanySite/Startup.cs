9fc1dfa4e4e429403349e81cd10bef7d7897dd8e
Katou <fintop1337@gmail.com>
2020-10-15 00:04:03 +0300

    Загрузил пакеты nuget. Добавил контроллер, закинул скрипты в 1 json файл

--- CompanySite/Startup.cs
+++ CompanySite/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,13 +13,14 @@ namespace CompanySite
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //поддержка controllers и views
+            services.AddControllersWithViews()
+                //совместимость asp core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,14 +28,15 @@ namespace CompanySite
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseRouting(); // подключение системы маршрутизаций
 
+            //статичные файлы
+            app.UseStaticFiles();
+
+            //маршруты 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
